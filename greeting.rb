5b5b4406a3ceed7bbb15a0b0b8b3f1e9129406ec
Sara Tarnvik <stvik2@gmail.com>
2019-11-04 00:33:37 +0000

    Automatically backed up by Learn

--- greeting.rb
+++ greeting.rb
@@ -1,14 +1,1 @@
-def greeting
-  puts "Hello world"
-end
-
-arr = (0..100).to_a
-
-print arr.select(&:odd?)
-puts "*******************************************"
-print arr.select(&:even?)
-
-
-print arr.map {|x|x**2}
-
-puts arr.find {|x| x**2 
+puts a.ord
